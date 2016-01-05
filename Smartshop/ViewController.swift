0b5bd35f634b828a029ecd2c71555a645a4d54f4
Shunchao Wang <wang.2799@osu.edu>
2016-01-05 14:17:35 -0500

    Initial import.

---
+++ Smartshop/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Smartshop
+//
+//  Created by Shunchao Wang on 1/5/2016.
+//  Copyright © 2016 swang. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
